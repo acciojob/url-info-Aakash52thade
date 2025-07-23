e9e588c9c37fcf4206974373b79209755c8ff343
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-23 22:32:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
+
 //your JS code here. If required.
-
 const URL = window.location.href;
-let count = URL.length();
+let count = URL.length;
 
-alert("The length of the URL is: "+ count);
+alert("The length of the URL is: " + count);
+
